b7a04be73e67f0db4ee1760400d590bd1eb41b26
prashant kumar <prashantkum7676@gmail.com>
2025-10-15 21:49:49 +0530

    css

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -11,7 +11,7 @@ export default function LandingPage() {
         <div className='landingPageContainer'>
             <nav>
                 <div className='navHeader'>
-                    <h2>Apna Video Call</h2>
+                    <h2>TalkieTime</h2>
                 </div>
                 <div className='navlist'>
                     <p onClick={() => {
@@ -35,7 +35,7 @@ export default function LandingPage() {
                 <div>
                     <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
 
-                    <p>Cover a distance by Apna Video Call</p>
+                    <p>Cover a distance by TalkieTime</p>
                     <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
